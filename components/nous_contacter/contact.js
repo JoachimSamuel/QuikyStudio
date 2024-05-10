f448f11aacdfd520d44ec4dac32a313df51790bb
Joachim Samuel <joachimsamuel@gmail.com>
2024-05-10 20:15:48 +0200

    index.html ok an statique manque encore la logique des formulaire est les slider a revoir

---
+++ components/nous_contacter/contact.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("components/nous_contacter/contact.html")
+    .then((response) => response.text())
+    .then((data) => {
+      // Sélectionnez l'élément où vous voulez insérer le footer
+      const targetElement = document.querySelector("#contac-section");
+
+      // Insérez le contenu HTML récupéré à l'intérieur de l'élément cible
+      targetElement.innerHTML = data;
+    })
+    .catch((error) =>
+      console.error("Erreur lors du chargement du footer:", error)
+    );
+});
