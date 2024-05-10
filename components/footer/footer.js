0c457c8d10dade2870b6163b16849f8c91ad4996
Joachim Samuel <joachimsamuel@gmail.com>
2024-05-10 18:17:29 +0200

    Footer Ok

---
+++ components/footer/footer.js
@@ -0,0 +1,11 @@
+/* Affiche le footer sur toutes les pages */
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("components/footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("beforeend", data);
+    })
+    .catch((error) =>
+      console.error("Erreur lors du chargement du footer:", error)
+    );
+});
